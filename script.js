dd103ed092647b37a49aa4725912c76fb76ec9e4
Abel Sassu <relinquished@hotmail.it>
2023-12-07 17:47:54 +0100

    aggiunte funzionalità per cambio pagina col button

--- script.js
+++ script.js
@@ -1,48 +1,60 @@
-function loadImages(query) {
+let currentPage = 1;
+function loadImages(query, page = 1) {
     const apiKey = "cs4zTIKddlPonimbtZtL98QOQHBePft3whyVBPhXEztTMtxhunkBLJPR";
-    const apiUrl = `https://api.pexels.com/v1/search?query=${query}`;
+    const apiUrl = `https://api.pexels.com/v1/search?query=${query}&page=${page}`;
+    
     fetch(apiUrl, {
-      method: 'GET',
-      headers: {
-        'Authorization': apiKey,
-      },
+        method: 'GET',
+        headers: {
+            'Authorization': apiKey,
+        },
     })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
         const cardContainer = document.querySelector(".album .container .row");
         cardContainer.innerHTML = "";
 
         data.photos.forEach((photo, index) => {
             const imageUrl = photo.src.large2x;
             if (imageUrl) {
-              const cardId = `card${index + 1}`;
-              const imageId = `image${index + 1}`;
-              const cardElement = createCard({ title: photo.photographer, text: photo.url }, cardId, imageId, imageUrl);
-              cardContainer.appendChild(cardElement);
+                const cardId = `card${index + 1}`;
+                const imageId = `image${index + 1}`;
+                const cardElement = createCard({ title: photo.photographer, text: photo.url }, cardId, imageId, imageUrl);
+                cardContainer.appendChild(cardElement);
             }
-          });
-      })
-      .catch(error => {
+        });
+    })
+    .catch(error => {
         console.error('Errore nella richiesta:', error);
-      });
-  }
+    });
+}
 
 
-  document.getElementById('loadImagesBtn').addEventListener('click', function() {
+document.addEventListener('DOMContentLoaded', function () {
     loadImages('people');
-  });
 
 
-  document.getElementById('loadSecondaryImagesBtn').addEventListener('click', function() {
-    loadImages('your-secondary-query');
-  });
-  document.getElementById('searchButton').addEventListener('click', function() {
+    document.getElementById('loadImagesBtn').addEventListener('click', function () {
+
+        currentPage++;
+        loadImages('people', currentPage);
+    });
+
+});
+
+document.getElementById('loadSecondaryImagesBtn').addEventListener('click', function () {
+    currentPage++;
+    loadImages('your-secondary-query', currentPage);
+});
+
+document.getElementById('searchButton').addEventListener('click', function () {
     const searchInputValue = document.getElementById('searchInput').value;
     if (searchInputValue.trim() !== '') {
-      loadImages(searchInputValue);
+        currentPage = 1;
+        loadImages(searchInputValue, currentPage);
     }
-  });
+});
 
   function hideCard(card) {
     if (card) {
